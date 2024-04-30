084331f69322583403be4f15810ebeda24d46e82
Murat Ozcan <muratkerem@gmail.com>
2024-04-30 09:49:58 -0500

    used templating and added examples

--- cypress/support/factories.ts
+++ cypress/support/factories.ts
@@ -35,21 +35,22 @@ export const generateCrocodile = (): CrocodileBase => {
 }
 
 export const generateUser = () => {
-  if (getEnvironment() !== 'local') {
-    return {
-      username: faker.internet.userName(),
-      first_name: faker.person.firstName(),
-      last_name: faker.person.lastName(),
-      email: faker.internet.email(),
-      password: faker.internet.password()
-    }
-  } else {
-    // use fixed data for local
-    return {
-      username: 'Tessie.Walker',
-      first_name: 'Samir',
-      last_name: 'Lakin',
-      email: 'Newton_Waters@hotmail.com'
-    }
+  // if (getEnvironment() !== 'local') {
+  return {
+    username: faker.internet.userName(),
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
   }
+  // }
+  // else {
+  //   // use fixed data for local
+  //   return {
+  //     username: 'Tessie.Walker',
+  //     first_name: 'Samir',
+  //     last_name: 'Lakin',
+  //     email: 'Newton_Waters@hotmail.com'
+  //   }
+  // }
 }
